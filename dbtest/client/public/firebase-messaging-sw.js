297c88ffc15e4bc7c8a5439c4ca6b32f1331cdb6
Karthik <84001885+JokerBroker@users.noreply.github.com>
2022-12-20 07:23:26 +0530

    Update firebase-messaging-sw.js

--- dbtest/client/public/firebase-messaging-sw.js
+++ dbtest/client/public/firebase-messaging-sw.js
@@ -4,13 +4,6 @@ importScripts(
 );
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCcEwIVresKxy1QnxgadQQCkVJgc7xzTBY",
-  authDomain: "fir-appspring.firebaseapp.com",
-  projectId: "fir-appspring",
-  storageBucket: "fir-appspring.appspot.com",
-  messagingSenderId: "169628979852",
-  appId: "1:169628979852:web:3d3a1837beccc4b106e1a9",
-  measurementId: "G-FZ0286XMDD"
 };
 
 firebase.initializeApp(firebaseConfig);
